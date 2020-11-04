cf8e47d57b6b99c26255f4d49f2d48e7da56cf65
ZaiZaiLo <893049558@qq.com>
2020-11-04 14:53:44 +0800

    First Commit

--- demo/src/main/java/com/zaizai/demo/entity/vo/AddUserVo.java
+++
@@ -1,9 +0,0 @@
-package com.zaizai.demo.entity.vo;
-
-/**
- * @Decription
- * @Author ZaiZai
- * @Date 2020/11/4 14:26
- */
-public class AddUserVo {
-}
--- demo/src/main/java/com/zaizai/demo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.zaizai.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zaizai.demo.entity.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
