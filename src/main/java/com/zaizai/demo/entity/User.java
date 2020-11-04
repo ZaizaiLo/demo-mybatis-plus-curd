04d88abbc0ced6d49a3dcfa5a3c61593530b350c
ZaiZaiLo <893049558@qq.com>
2020-11-04 14:55:41 +0800

    First Commit

---
+++ src/main/java/com/zaizai/demo/entity/User.java
@@ -0,0 +1,67 @@
+package com.zaizai.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ApiModel(value = "用户信息实体")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "demo_tb_user")
+public class User {
+    /**
+     * 编号
+     */
+    @TableId(value = "ID", type = IdType.INPUT)
+    @ApiModelProperty(value = "编号")
+    private String id;
+
+    /**
+     * 用户名
+     */
+    @TableField(value = "USER_NAME")
+    @ApiModelProperty(value = "用户名")
+    private String userName;
+
+    /**
+     * 性别
+     */
+    @TableField(value = "USER_GENDER")
+    @ApiModelProperty(value = "性别")
+    private String userGender;
+
+    /**
+     * 昵称
+     */
+    @TableField(value = "NICKNAME")
+    @ApiModelProperty(value = "昵称")
+    private String nickname;
+
+    /**
+     * 邮箱
+     */
+    @TableField(value = "USER_EMAIL")
+    @ApiModelProperty(value = "邮箱")
+    private String userEmail;
+
+    /**
+     * 手机号码
+     */
+    @TableField(value = "USER_PHONE")
+    @ApiModelProperty(value = "手机号码")
+    private String userPhone;
+
+    /**
+     * 密码
+     */
+    @TableField(value = "`PASSWORD`")
+    @ApiModelProperty(value = "密码")
+    private String password;
+}
---
+++ src/main/java/com/zaizai/demo/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zaizai.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zaizai.demo.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
