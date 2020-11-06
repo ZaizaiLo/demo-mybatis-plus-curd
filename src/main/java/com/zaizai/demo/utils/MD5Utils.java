2bd042abae03635b12518b6122a0f5f6beef9bfc
ZaiZaiLo <893049558@qq.com>
2020-11-06 10:08:05 +0800

    添加用户接口

--- src/main/java/com/zaizai/demo/utils/MD5Utils.java
+++ src/main/java/com/zaizai/demo/utils/MD5Utils.java
@@ -16,7 +16,6 @@ public class MD5Utils {
 
     public static String inputPassToFormPass(String inputPass) {
         String str = "" + salt.charAt(0) + salt.charAt(2) + inputPass + salt.charAt(5) + salt.charAt(4);
-        System.out.println(str);
         return md5(str);
     }
 
